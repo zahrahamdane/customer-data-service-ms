a68dd1ba508ba18d9fd20db5e87cb88961812050
Zahra Goarant <zahra.goarant@externe.systeme-u.fr>
2024-12-20 02:41:11 +0100

    first commit

---
+++ src/main/java/com/zaradev/customerdataservicems/CustomerDataServiceMsApplication.java
@@ -0,0 +1,13 @@
+package com.zaradev.customerdataservicems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDataServiceMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerDataServiceMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zaradev/customerdataservicems/CustomerDataServiceMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaradev.customerdataservicems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDataServiceMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
